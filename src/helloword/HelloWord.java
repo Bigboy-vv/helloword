ae9016881284478f89aa70bf2f4f89e1092fbf7b
Administrator <Administrator@EMPJBNHI7WPRIOC>
2019-12-11 14:17:27 +0800

    test

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -2,6 +2,8 @@ package helloword;
 
 public class HelloWord {
   public static void main(String[] args) {
-	 System.out.println("holle zhao");
+	 System.out.println("zhao");
+	 System.out.println("zhu");
+	 System.out.println("zhang");
 }
 }
